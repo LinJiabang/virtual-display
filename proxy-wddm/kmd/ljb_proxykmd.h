3a589ff10bfcd3c1f9c7782b883484d37c6f3913
LinJiabang <lin.jiabang@gmail.com>
2016-01-24 00:23:35 +0800

    update ljb_proxykmd.h

--- proxy-wddm/kmd/ljb_proxykmd.h
+++ proxy-wddm/kmd/ljb_proxykmd.h
@@ -1,3 +1,23 @@
+/*
+ * ljb_proxykmd.h
+ *
+ * Author: Lin Jiabang (lin.jiabang@gmail.com)
+ *     Copyright (C) 2016  Lin Jiabang
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #ifndef _LJB_PROXYKMD_H_
 #define _LJB_PROXYKMD_H_
@@ -7,12 +27,61 @@
 
 #pragma warning(disable:4201) /* allow nameless struct/union */
 
-
 #define DXGKRNL_SYS_STR                 L"dxgkrnl.sys"
 #define USER_MODE_DRIVER_NAME           L"UserModeDriverName"
 #define USER_MODE_DRIVER_NAME_WOW       L"UserModeDriverNameWow"
 
 #define MY_USER_MODE_DRIVER_NAME        L"ljb_umd.dll\0ljb_umd.dll\0ljb_umd.dll\0"
 #define MY_USER_MODE_DRIVER_NAME_WOW    L"ljb_umd32.dll\0ljb_umd32.dll\0ljb_umd32.dll\0"
+
+typedef enum _DEVICE_TYPE
+    {
+    DEVICE_TYPE_INVALID,
+    DEVICE_TYPE_FDO,
+    DEVICE_TYPE_FILTER
+    } DEVICE_TYPE;
+
+typedef enum _PNP_STATE
+    {
+    PNP_NOT_STARTED,
+    PNP_STARTED,
+    PNP_STOP_PENDING,
+    PNP_STOPPED,
+    PNP_REMOVE_PENDING,
+    PNP_SURPRISE_REMOVE_PENDING,
+    PNP_DELETED
+    } PNP_STATE;
+
+#define INITIALIZE_PNP_STATE(_Data_)    \
+        (_Data_)->DevicePnPState =  PNP_NOT_STARTED;\
+        (_Data_)->PreviousPnPState = PNP_NOT_STARTED;
+
+#define SET_NEW_PNP_STATE(_Data_, _state_) \
+        (_Data_)->PreviousPnPState =  (_Data_)->DevicePnPState;\
+        (_Data_)->DevicePnPState = (_state_);
+
+#define RESTORE_PREVIOUS_PNP_STATE(_Data_)   \
+        (_Data_)->DevicePnPState =   (_Data_)->PreviousPnPState;\
+
+
+typedef struct _LJB_DEVICE_EXTENSION
+{
+    DEVICE_OBJECT *                     DeviceObject;
+    DEVICE_OBJECT *                     NextLowerDriver;
+    DEVICE_OBJECT *                     PhysicalDeviceObject;
+    DEVICE_OBJECT *                     FilterDeviceObject;
+    RTL_OSVERSIONINFOW                  RtlOsVersion;
+
+    DEVICE_TYPE                         DeviceType;
+    PNP_STATE                           DevicePnPState;
+    PNP_STATE                           PreviousPnpState;
+    IO_REMOVE_LOCK                      RemoveLock;
+
+    PFN_DXGK_INITIALIZE                 pfnDxgkInialize;
+    PFILE_OBJECT                        DxgkFileObject;
+
+    ULONG                               DebugLevel;
+    } LJB_DEVICE_EXTENSION;
+
 
 #endif
--- proxy-wddm/kmd/ljb_proxykmd_driver_entry.c
+++ proxy-wddm/kmd/ljb_proxykmd_driver_entry.c
@@ -1,3 +1,24 @@
+/*
+ * ljb_proxykmd_driver_entry.c
+ *
+ * Author: Lin Jiabang (lin.jiabang@gmail.com)
+ *     Copyright (C) 2016  Lin Jiabang
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include "ljb_proxykmd.h"
 
 #ifdef ALLOC_PRAGMA
@@ -6,46 +27,46 @@
 
 /*
  * Name:  DriverEntry
- * 
+ *
  * Definition:
  *    NTSTATUS
  *    DriverEntry(
  *        __in PDRIVER_OBJECT  DriverObject,
  *        __in PUNICODE_STRING RegistryPath
  *        );
- * 
+ *
  * Description:
  *    DriverEntry initializes the driver and is the first routine called by the
  *    system after the driver is loaded.
- * 
+ *
  *    DriverObject - represents the instance of the function driver that is loaded
  *    into memory. DriverEntry must initialize members of DriverObject before it
  *    returns to the caller. DriverObject is allocated by the system before the
  *    driver is loaded, and it is released by the system after the system unloads
  *    the function driver from memory.
- * 
+ *
  *    RegistryPath - represents the driver specific path in the Registry.
  *    The function driver can use the path to store driver related data between
  *    reboots. The path does not store hardware instance specific data.
- * 
+ *
  * Return Value:
  *    DriverEntry returns STATUS_SUCCESS. NTSTATUS values are defined in the DDK
  *    header file, Ntstatus.h.
- * 
+ *
  *    Drivers execute in the context of other threads in the system. They do not
  *    receive their own threads. This is different than with applications. After
  *    DriverEntry returns, the Toaster function driver executes in an asynchronous,
  *    event driven, manner.
- * 
+ *
  *    The system schedules other threads to execute on behalf of the driver. Every
  *    thread has a user-mode stack and a kernel-mode stack. When the system schedules
  *    a thread to execute in a driver, that thread's kernel stack is used.
- * 
+ *
  *    The I/O manager calls the function driver's routines to process events and I/O
  *    operations as needed. Multiple threads can execute simultaneously throughout the
  *    driver, thus special attention to how the driver operates and processes events
  *    and I/O operations is required
- * 
+ *
  */
 
 NTSTATUS
@@ -58,7 +79,7 @@ DriverEntry(
 
     UNREFERENCED_PARAMETER(RegistryPath);
 
-	KdPrint((__FUNCTION__ ": Built %s %s\n", __DATE__, __TIME__));
+    KdPrint((__FUNCTION__ ": Built %s %s\n", __DATE__, __TIME__));
 
     RtlZeroMemory(&gDriverData, sizeof(gDriverData));
     InitializeListHead(&gDriverData.ClientDriverListHead);
