561c323d55ca520b5d9d14578f0e2af042ee475a
LinJiabang <lin.jiabang@gmail.com>
2016-09-28 18:46:10 +0800

    20160928

--- virtual_monitor/buildall_wdk10.cmd
+++ virtual_monitor/buildall_wdk10.cmd
@@ -1,15 +1,15 @@
 @echo off
-nmake2msbuild DIRS -NoSolution -NoPackageProject
+REM nmake2msbuild DIRS -NoSolution -NoPackageProject
 
-REM msbuild bus\source\vmon_bus.vcxproj /p:Configuration=Debug;Platform=Win32;TargetOsVersion=Win7;EnableInf2cat=false
-REM msbuild bus\source\vmon_bus.vcxproj /p:Configuration=Release;Platform=Win32;TargetOsVersion=Win7;EnableInf2cat=false
-REM msbuild bus\source\vmon_bus.vcxproj /p:Configuration=Debug;Platform=x64;TargetOsVersion=Win7;EnableInf2cat=false
-REM msbuild bus\source\vmon_bus.vcxproj /p:Configuration=Release;Platform=x64;TargetOsVersion=Win7;EnableInf2cat=false
-REM 
-REM msbuild func\source\vmon_func.vcxproj /p:Configuration=Debug;Platform=Win32;TargetOsVersion=Win7;EnableInf2cat=false
-REM msbuild func\source\vmon_func.vcxproj /p:Configuration=Release;Platform=Win32;TargetOsVersion=Win7;EnableInf2cat=false
-REM msbuild func\source\vmon_func.vcxproj /p:Configuration=Debug;Platform=x64;TargetOsVersion=Win7;EnableInf2cat=false
-REM msbuild func\source\vmon_func.vcxproj /p:Configuration=Release;Platform=x64;TargetOsVersion=Win7;EnableInf2cat=false
+msbuild bus\source\vmon_bus.vcxproj /p:Configuration=Debug;Platform=Win32;TargetOsVersion=Win7;EnableInf2cat=false
+msbuild bus\source\vmon_bus.vcxproj /p:Configuration=Release;Platform=Win32;TargetOsVersion=Win7;EnableInf2cat=false
+msbuild bus\source\vmon_bus.vcxproj /p:Configuration=Debug;Platform=x64;TargetOsVersion=Win7;EnableInf2cat=false
+msbuild bus\source\vmon_bus.vcxproj /p:Configuration=Release;Platform=x64;TargetOsVersion=Win7;EnableInf2cat=false
+
+msbuild func\source\vmon_func.vcxproj /p:Configuration=Debug;Platform=Win32;TargetOsVersion=Win7;EnableInf2cat=false
+msbuild func\source\vmon_func.vcxproj /p:Configuration=Release;Platform=Win32;TargetOsVersion=Win7;EnableInf2cat=false
+msbuild func\source\vmon_func.vcxproj /p:Configuration=Debug;Platform=x64;TargetOsVersion=Win7;EnableInf2cat=false
+msbuild func\source\vmon_func.vcxproj /p:Configuration=Release;Platform=x64;TargetOsVersion=Win7;EnableInf2cat=false
 
 msbuild notify\source\notify.vcxproj /p:Configuration=Debug;Platform=Win32;TargetOsVersion=Win7;EnableInf2cat=false
 msbuild notify\source\notify.vcxproj /p:Configuration=Release;Platform=Win32;TargetOsVersion=Win7;EnableInf2cat=false
@@ -83,8 +83,8 @@ copy notify\source\Release\x64\notify.pdb       %OUTPUT%\pdb\fre\x64\
 REM
 REM run INF2CAT
 REM
-INF2CAT /uselocaltime /drv:.\%OUTPUT%\package\chk /os:Vista_X86,Vista_X64,7_X86,7_X64
-INF2CAT /uselocaltime /drv:.\%OUTPUT%\package\fre /os:Vista_X86,Vista_X64,7_X86,7_X64
+INF2CAT /uselocaltime /drv:%OUTPUT%\package\chk /os:7_X86,7_X64,8_X86,8_X64,10_X86,10_X64
+INF2CAT /uselocaltime /drv:%OUTPUT%\package\fre /os:7_X86,7_X64,8_X86,8_X64,10_X86,10_X64
 
 if "%TEST_SIGN%" == "" set TEST_SIGN=signtool.exe sign /v /s trustedpublisher /n ljbtest /t http://timestamp.verisign.com/scripts/timstamp.dll
 
